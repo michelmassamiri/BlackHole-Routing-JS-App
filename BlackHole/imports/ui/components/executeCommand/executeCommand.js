32caad4785223fe661caac54c087d2172dac92b0
Michel Massamiri <mmassamire@services.emi.u-bordeaux.fr>
2018-02-21 22:40:45 +0100

    Command form will appear if the button selectionner is pushed

--- BlackHole/imports/ui/components/executeCommand/executeCommand.js
+++ BlackHole/imports/ui/components/executeCommand/executeCommand.js
@@ -1,1 +1,25 @@
+import './addRoute/addRoute.js';
 import './executeCommand.html';
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+Template.executeCommand.onCreated(function returnTemplate(){
+   this.command_template = new ReactiveVar("");
+});
+
+Template.executeCommand.helpers({
+    showCommandTemplate(){
+        const instance = Template.instance();
+        const template_name = instance.command_template.get();
+
+        return template_name;
+    }
+});
+
+Template.executeCommand.events({
+   'click #submit-btn'(event, instance) {
+        const template_value = instance.find('#command').value;
+        instance.command_template.set(template_value);
+    },
+});
