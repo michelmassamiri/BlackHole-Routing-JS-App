54d50417e1c4c3d3d878c6cc9f6dcfa9fc6c7f80
Michel Massamiri <mmassamire@services.emi.u-bordeaux.fr>
2018-02-21 22:41:16 +0100

    Fixing the HTML and the CSS design

--- BlackHole/imports/ui/components/appContent/appContent.js
+++ BlackHole/imports/ui/components/appContent/appContent.js
@@ -1,7 +1,4 @@
-import { Template } from 'meteor/templating';
-
 import '../executeCommand/executeCommand.js';
-import '../addRoute/addRoute.js';
 import '../listCommand/listCommand.js';
 import '../listRoutes/listRoutes.js';
 import './appContent.html';
